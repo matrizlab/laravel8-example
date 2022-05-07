c07bb57f44264c15ca707e8731f2ce519034534d
matrizlab <info@matrizlab.com>
2022-05-07 09:49:59 +0200

    add login

--- routes/api.php
+++ routes/api.php
@@ -29,4 +29,5 @@ use Illuminate\Support\Facades\Route;
 //Route::delete('users/{id}', '\App\Http\Controllers\UserController@destroy');
 
 Route::apiResource('users', '\App\Http\Controllers\UserController');
+Route::post('login', '\App\Http\Controllers\AuthController@login');
 
