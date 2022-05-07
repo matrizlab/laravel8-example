b3f13c29d9b47e2c57e667726c54886992a7487b
matrizlab <info@matrizlab.com>
2022-05-07 08:53:21 +0200

    add user pagination

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,8 @@ class UserController extends Controller
 
     public function index()
     {
-        return User::all();
+        //return User::all();
+        return User::paginate();
     }
 
     public function show($id)
